03b9e0ef96856b2dcd8c148451b033a21e49d104
Larinha200 <laravitoriaqueiroz4@gmail.com>
2025-10-21 08:14:17 -0300

    def menu

---
+++ funcion.py
@@ -0,0 +1,4 @@
+def menu_login():
+    return "1- Cadastro \n2- Login na conta corrente \n3- Login na conta poupança"
+def menu():
+    return "1- Cadastro \n3- Contas \n4- Depósito \n5- Saque \n6- Tranferência entre contas \n7- Conslta de saldo \n8- Consulta de extrato "
