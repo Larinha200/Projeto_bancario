395c618fe05938d64d6d5bcd3261aa3f656d377c
Larinha200 <laravitoriaqueiroz4@gmail.com>
2025-10-21 10:25:25 -0300

    menu

--- app.py
+++ app.py
@@ -3,54 +3,54 @@ from funcion import*
 
 
 clientes = {}
-match menu_login():
-    
-    case 1:
-        menu_cadastro(clientes)
+while True:
+    match menu_login():
         
-        
-    case 2:
-        
-        match  menu_principal():
-            case 1:
-                pass
-            case 2:
-                pass
-            case 3:
-                pass
-            case 4:
-                pass
-            case 5:
-                pass
-            case 6:
-                pass
-            case 7:
-                pass
-            case 8:
-                pass
-            case _:
-                pass
-        
-    case 3:
-         match  resp:
-            case 1:
-                pass
-            case 2:
-                pass
-            case 3:
-                pass
-            case 4:
-                pass
-            case 5:
-                pass
-            case 6:
-                pass
-            case 7:
-                pass
-            case 8:
-                pass
-            case _:
-                pass
-        
-    case _ :
-        pass
+        case 1:
+            menu_cadastro(clientes)
+            
+        case 2:
+            
+            match  menu_principal():
+                case 1:
+                    pass
+                case 2:
+                    pass
+                case 3:
+                    pass
+                case 4:
+                    pass
+                case 5:
+                    pass
+                case 6:
+                    pass
+                case 7:
+                    pass
+                case 8:
+                    pass
+                case _:
+                    pass
+            
+        case 3:
+            match menu_principal():
+                case 1:
+                    pass
+                case 2:
+                    pass
+                case 3:
+                    pass
+                case 4:
+                    pass
+                case 5:
+                    pass
+                case 6:
+                    pass
+                case 7:
+                    pass
+                case 8:
+                    pass
+                case _:
+                    pass
+            
+        case _ :
+            pass
