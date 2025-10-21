70c196b3d526421c3eb3e094ee6740b635e7b665
Katrisbug <bonetkatrina@gmail.com>
2025-10-21 08:39:16 -0300

    atributos, gets e add conta do cliente

--- classes.py
+++ classes.py
@@ -4,11 +4,29 @@ from abc import ABC, abstractmethod #importar a biblioteca ABC
 class Banco:
     pass
 
-class Cliente:
-    pass
-
 class Conta(ABC):
     pass
+
+class Cliente(Conta, ABC):
+    def __init__(self, nome: str, cpf):
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__contas = []
+
+#GETS para acessar atributos privados da classe cliente
+    def getNome(self):
+        return self.__nome
+    
+    def getCpf(self):
+        return self.__cpf
+    
+    def getContas(self):
+        return self.__contas
+    
+    #Def para adicionar conta
+    def add_conta(self, conta):
+        return self.__contas.append(conta)
+
 
 class Extrato:
     pass
